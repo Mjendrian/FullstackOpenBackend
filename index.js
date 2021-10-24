cd8b27b15d3b985574ca66f2a7eb1f2ccd10d010
MJendrian <mjendrian@gmail.com>
2021-10-24 13:25:28 +0200

    Added database functionality to all actions

--- index.js
+++ index.js
@@ -16,47 +16,52 @@ const morganFormat = ':method :url :status :res[content-length] - :response-time
 
 app.use(morgan(morganFormat))
 
-  // Middleware : Catch requests without route
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
 // Info request
-app.get('/info', (request, response) => {
-    const returnString = `
-    <p>The phonebook contains ${persons.length} contacts</p>
-    <p>The request was received at ${new Date}</p>`
-    response.send(returnString)   
+app.get('/info', (request, response, next) => {
+    persons = Person.find({})
+    .then(persons => {
+      const returnString = `
+      <p>The phonebook contains ${persons.length} contacts</p>
+      <p>The request was received at ${new Date}</p>`
+      response.send(returnString)
+    })
+    .catch(error => next(error))
+       
 })
 
 // Phonebook list
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+  .then(persons => {
     response.json(persons)
   })
+  .catch(error => next(error)) 
 })
 
 // Phonebook entry show
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {    
-        response.json(person)  
-    } else {    
-        response.status(404).end()  
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+  .then(person => {
+    if (person) {        
+      response.json(person)      
+    } else {        
+      response.status(404).end()      
+    }    
+  })
+  .catch(error => next(error))   
 })
 
 // Phonebook delete entry
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 // Phonebook add entry
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || body.name === "") {
@@ -76,12 +81,51 @@ app.post('/api/persons', (request, response) => {
       number: body.number
     })
   
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
   })
 
-app.use(unknownEndpoint)
+  // Phonebook update entry
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  if (!body.number || body.number === "") {
+    return response.status(400).json({ 
+      error: 'Number missing' 
+    })
+  }
+
+  const person = {
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatePerson => {
+      response.json(updatePerson)
+    })
+    .catch(error => next(error))
+})
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+  app.use(unknownEndpoint)
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
